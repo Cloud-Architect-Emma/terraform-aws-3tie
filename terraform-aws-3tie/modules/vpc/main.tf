251050ff0249a9684e7e64b8a0e3fa46ffd9ce0d
EMMANUELA OPURUM <86380966+Cloud-Architect-Emma@users.noreply.github.com>
2025-11-17 02:28:05 +0000

    Add VPC and public subnet resources
    Define main VPC and a public subnet in Terraform.

---
+++ terraform-aws-3tie/modules/vpc/main.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+}
+
+# Add private subnets, IGW, NAT, security groups (allow HTTP/HTTPS, SSH from your IP)
